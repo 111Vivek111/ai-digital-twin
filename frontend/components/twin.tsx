0e5d28779666ae7768647664d775ec21524af858
111Vivek111 <prajapativivek93165@gmail.com>
2026-06-25 16:13:47 +0530

    deployed successfully

--- frontend/components/twin.tsx
+++ frontend/components/twin.tsx
@@ -40,7 +40,7 @@ export default function Twin() {
         setIsLoading(true);
 
         try {
-            const response = await fetch('http://localhost:8000/chat', {
+            const response = await fetch('https://8k6b2dsqcj.execute-api.ap-south-1.amazonaws.com/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
